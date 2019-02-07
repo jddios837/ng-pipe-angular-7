73f46fd0b9e259a8bddfb00bfa5711d926aa7e00
Juan Gomez <zatkiel837@gmail.com>
2019-02-07 07:26:02 -0500

    pipe in component code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,42 @@
 import { Component, OnInit } from '@angular/core';
 
+import { FileSizePipe } from "./pipes/filesize.pipe";
+
 interface File {
   name: string,
-  size: number,
+  size: any,
   type: string
 }
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [FileSizePipe]
 })
 export class AppComponent implements OnInit {
   title = 'ng-pipes';
 
+  constructor(
+    private fileSizePipe: FileSizePipe
+  ) {}
+
   files: File[];
+  mapped: File[];
   ngOnInit() {
     this.files = [
       { name: 'logo.svg', size: 2120109, type: 'image/svg' },
       { name: 'banner.jpg', size: 18029, type: 'image/jpg' },
       { name: 'background.png', size: 1784562, type: 'image/png' }
     ];
+
+    this.mapped = this.files.map(file => {
+      return {
+        name: file.name,
+        type: file.type,
+        size: this.fileSizePipe.transform(file.size, 'mb')
+      }
+    })
   }
 }
